fb81ca3eabed7d92bce26595cebb2f7b7e882c96
boris <borislav2003@gmail.com>
2022-12-29 15:13:14 +0500

    added reset css, created getting companies code and companies template

--- src/app/modules/companies/components/companies/companies.component.ts
+++ src/app/modules/companies/components/companies/companies.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CompaniesService } from '../../services/companies.service';
+import { Observable } from 'rxjs';
+import { ICompany } from '../../models/company.interface';
 
 @Component({
   selector: 'app-companies',
@@ -7,4 +9,16 @@ import { CompaniesService } from '../../services/companies.service';
   styleUrls: ['./companies.component.scss'],
   providers: [CompaniesService],
 })
-export class CompaniesComponent {}
+export class CompaniesComponent implements OnInit {
+  companies$: Observable<ICompany[]> | undefined;
+  public pageSize: number = 16;
+  constructor(private readonly companiesService: CompaniesService) {}
+
+  getCompanies(): void {
+    this.companies$ = this.companiesService.getCompanies(this.pageSize);
+  }
+
+  ngOnInit(): void {
+    this.getCompanies();
+  }
+}
