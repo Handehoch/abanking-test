47b335084b84adc0c2479450c7661c415d7f0841
Borislav <borislav2003@gmail.com>
2023-01-03 15:50:31 +0400

    companies filtering by name, type and activity type

--- src/app/modules/companies/companies.module.ts
+++ src/app/modules/companies/companies.module.ts
@@ -6,6 +6,7 @@ import { CompanyComponent } from './components/company/company.component';
 import { AppRoutingModule } from '../routing/app-routing.module';
 import { CompaniesService } from './services/companies.service';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { CompaniesFilterPipe } from '../../shared/pipes/companies-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeaderComponent } from '../../shared/components/header/header.component
     CompanyCardComponent,
     CompanyComponent,
     HeaderComponent,
+    CompaniesFilterPipe,
   ],
   imports: [CommonModule, AppRoutingModule],
   providers: [
